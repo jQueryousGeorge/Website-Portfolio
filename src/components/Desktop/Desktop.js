7c30a3205e63737c7eefa57b505951e5440e19d0
Tyler <tyler.s9@proton.me>
2025-05-22 19:28:18 -0400

    Initial commit: Windows 95 themed portfolio
    - Set up React app with sass, redux toolkit, and react-router
    - Created Desktop component with authentic Windows 95 teal background
    - Added Windows 95 color system variables (authentic hex values)
    - Established component folder structure for Desktop and icons
    - Ready to build nostalgic UI components (taskbar, windows, icons)
    
    This portfolio recreates the Windows 95 experience as a unique way
    to showcase my projects.

---
+++ src/components/Desktop/Desktop.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Desktop.scss';
+
+const Desktop = () => {
+    return (
+        <div className='desktop'>
+            {/* { Desktop items will go here} */}
+        </div>
+    );
+};
