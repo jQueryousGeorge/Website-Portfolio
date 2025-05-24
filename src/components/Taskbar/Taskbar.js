92b25d74306dd97c228e3e09233d95e634f0e19e
Tyler <tyler.s9@proton.me>
2025-05-23 21:07:10 -0400

    Had to change my notes directory from hidden to a normal direcotry so it would be git-ignored. Added the import statements of SystemTray and StartButton to /src/components/Taskbar/Taskbar.js

--- src/components/Taskbar/Taskbar.js
+++ src/components/Taskbar/Taskbar.js
@@ -1,3 +1,4 @@
 import React from "react";
 import './Taskbar.scss';
-import StartButton from './Components/StartButton';
+import StartButton from './Components/StartButton/StartButton';
+import SystemTray from './components/SystemTray/SystemTray';
