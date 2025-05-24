436efcc0cfab442403f1fc6525a1710d22d04ce8
Tyler <tyler.s9@proton.me>
2025-05-23 21:32:56 -0400

    Finished adding the basics of Taskbar.js: - imported SystemTray and StartButton - Added <div>'s for the future tasks (applications) that will appear on the Taskbar.

--- src/components/Taskbar/Taskbar.js
+++ src/components/Taskbar/Taskbar.js
@@ -2,3 +2,17 @@ import React from "react";
 import './Taskbar.scss';
 import StartButton from './Components/StartButton/StartButton';
 import SystemTray from './components/SystemTray/SystemTray';
+
+const Taskbar = () => {
+    return (
+        <div className="taskbar">
+            <StartButton />
+            <div className="taskabr_tasks">
+                {/* Open apps will pop-up here on the Taskbar */}
+            </div>
+            <SystemTray />
+        </div>
+    );
+};
+
+export default Taskbar;
