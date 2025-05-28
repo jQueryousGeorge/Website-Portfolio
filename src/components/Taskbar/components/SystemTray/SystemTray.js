0c18bee1c05343bc9a5a95857ddd0b825b13720e
Tyler <tyler.s9@proton.me>
2025-05-28 12:53:13 -0400

    Working on adding the clock tray to the SystemTray

--- src/components/Taskbar/components/SystemTray/SystemTray.js
+++ src/components/Taskbar/components/SystemTray/SystemTray.js
@@ -1,9 +1,12 @@
 import React from "react";
+import './SystemTray.scss';
 
 const SystemTray = () => {
     return (
         <div className="systemtray-right">
-            {/* SystemTray logic */}
+            <div className="tray-clock">
+                <span className="clock-text">12:00</span>
+            </div>
         </div>
     );
 };
