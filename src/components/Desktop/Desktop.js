ad486da6497d7b7bfb118652bff1979436307324
Tyler <tyler.s9@proton.me>
2025-05-28 14:27:53 -0400

    Adding the icon library for the Desktop. - Added the initialDesktopIcons array to Desktop.js - Need to ponder on all apps I want to add - Will probably add React Components for the actual apps themselves

--- src/components/Desktop/Desktop.js
+++ src/components/Desktop/Desktop.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './Desktop.scss';
 import Taskbar from '../Taskbar/Taskbar';
 
+const initialDesktopIcons = [
+    {id: "xxx", label: "xxx", windowId: "xxx", contentType: "xxx"},
+    {id: "xxx", label: "xxx", windowId: "xxx", contentType: "xxx"},
+    {id: "xxx", label: "xxx", windowId: "xxx", contentType: "xxx"},
+    {id: "xxx", label: "xxx", windowId: "xxx", contentType: "xxx"},
+    {id: "xxx", label: "xxx", windowId: "xxx", contentType: "xxx"},
+    {id: "xxx", label: "xxx", windowId: "xxx", contentType: "xxx"}
+];
+
 const Desktop = () => {
     return (
         <div className='desktop'>
