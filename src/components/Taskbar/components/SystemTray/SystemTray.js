20d83827f41cdb6b121d2808f932acea17063b04
Tyler <tyler.s9@proton.me>
2025-05-28 13:24:26 -0400

    Added the current time to SystemTray using React's useEffect prop

--- src/components/Taskbar/components/SystemTray/SystemTray.js
+++ src/components/Taskbar/components/SystemTray/SystemTray.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import './SystemTray.scss';
 
 const SystemTray = () => {
+    const [currentTime, setCurrentTime] = useState('');
+
+    useEffect(() => {
+        const updateTime = () => {
+            const now = new Date();
+            let hours = now.getHours();
+            const minutes = now.getMinutes();
+            const ampm = hours >= 12 ? 'PM' : 'AM';
+            hours = hours % 12;
+            hours = hours ? hours : 12; // The hour '0' should be '12' am.
+            const minuteStr = minutes < 10 ? '0' + minutes : minutes;
+            setCurrentTime(`${hours}: ${minuteStr} ${ampm}`);
+        };
+
+        updateTime();
+        const timerId = setInterval(updateTime, 1000);
+
+        return () => {
+            clearInterval(timerId); // Cleanup interval on component unmounting
+        }
+    }, []);
+
+
     return (
         <div className="systemtray-right">
             <div className="tray-clock">
-                <span className="clock-text">12:00</span>
+                {currentTime}
             </div>
         </div>
     );
