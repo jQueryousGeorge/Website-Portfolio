89e522c36ca4e157530894339c93533f0e4c11ed
Tyler <tyler.s9@proton.me>
2025-05-31 20:04:32 -0400

    Added the desktop icons I think the Desktop should have - My Computer - Recycle Bin - Documents - About Me - Projects - Contact (me)

--- src/components/Desktop/Desktop.js
+++ src/components/Desktop/Desktop.js
@@ -3,12 +3,12 @@ import './Desktop.scss';
 import Taskbar from '../Taskbar/Taskbar';
 
 const initialDesktopIcons = [
-    {id: "xxx", label: "xxx", windowId: "xxx", contentType: "xxx"},
-    {id: "xxx", label: "xxx", windowId: "xxx", contentType: "xxx"},
-    {id: "xxx", label: "xxx", windowId: "xxx", contentType: "xxx"},
-    {id: "xxx", label: "xxx", windowId: "xxx", contentType: "xxx"},
-    {id: "xxx", label: "xxx", windowId: "xxx", contentType: "xxx"},
-    {id: "xxx", label: "xxx", windowId: "xxx", contentType: "xxx"}
+    { id: 'my-computer', label: 'My Computer', windowId: 'myComputerWindow', contentType: 'text', content: 'Contents of My Computer' },
+    { id: 'recycle-bin', label: 'Recycle Bin', windowId: 'recycleBinWindow', contentType: 'text', content: 'Contents of Recycle Bin' },
+    { id: 'documents', label: 'My Documents', windowId: 'myDocumentsWindow', contentType: 'text', content: 'Contents of My Documents' },
+    { id: 'about-me', label: 'About Me', windowId: 'aboutMeWindow', contentType: 'component', contentComponent: AboutMe },
+    { id: 'projects', label: 'Projects', windowId: 'projectsWindow', contentType: 'component', contentComponent: Projects },
+    { id: 'contact', label: 'Contact', windowId: 'contactWindow', contentType: 'component', contentComponent: Contact }
 ];
 
 const Desktop = () => {
