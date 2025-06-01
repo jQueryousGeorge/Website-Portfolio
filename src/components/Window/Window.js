6e46c5c0d02261971afb40c94e9a69751f909b24
Tyler <tyler.s9@proton.me>
2025-05-31 21:24:20 -0400

    Added the basic functionality of <Window/> component. - Desktop icons will be treated as Windows -- double clicking the icon will launch a Window with the app information

---
+++ src/components/Window/Window.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Window.scss";
+
+const Window = ({ title, isOpen }) => {
+    if (!isOpen) {
+        return null;
+    }
+
+    return (
+        <div className="window">
+            <div className="title-bar">
+                <div className="title-bar-text">Title</div>
+                <div className="title-bar-controls">
+                    <button
+                        aria-label="Minimize"
+                        className="title-bar-button minimize-button"
+                    ></button>
+                    <button
+                        aria-label="Maximize"
+                        className="title-bar-button maximize-button"
+                    ></button>
+                    <button
+                        aria-label="Close"
+                        className="title-bar-button close-button"
+                    ></button>
+                </div>
+            </div>
+            <div className="window-body">Children here...</div>
+        </div>
+    );
+};
+
+export default Window;
