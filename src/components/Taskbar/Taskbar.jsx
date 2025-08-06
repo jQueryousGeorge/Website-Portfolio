09c040a5340aa0a099f500d8075ca7462f629c6f
Tyler <tyler.s9@proton.me>
2025-08-05 19:00:13 -0700

    Adding the logic from Window && Desktop jsx to Taskbar jsx now.
    - Added handleTaskbarButtonClick to Taskbar jsx

--- src/components/Taskbar/Taskbar.jsx
+++ src/components/Taskbar/Taskbar.jsx
@@ -3,12 +3,26 @@ import "./Taskbar.scss";
 import StartButton from "./components/StartButton/StartButton";
 import SystemTray from "./components/SystemTray/SystemTray";
 
-const Taskbar = () => {
+const Taskbar = ({ openWindows = {}, onWindowFocus, onMinimizeWindow, activeWindowId }) => {
+    const handleTaskbarButtonClick = (windowId) => {
+        const window = openWindows[windowId];
+        if (window.isMinimized) {
+            // Restore window if minimized
+            onMinimizeWindow(windowId, false);
+        } else if (activeWindowId === windowId) {
+            // Minimize window if it's the active one
+            onMinimizeWindow(windowId, true);
+        } else {
+            // Focus the window
+            onWindowFocus(windowId);
+        }
+    };
+
     return (
         <div className="taskbar-bottom">
             {/* Main taskbar div - entire taskbar, used to position it at bottom */}
 
-            <div 
+            <div
                 className="taskbar-startbutton">
                 <StartButton />
             </div>
